0e52ff5c74c0fc757cf7944ca5ae1ab3fa03e2a0
heetop <heetop@jeonghuitag-ui-MacBook-Pro.local>
2017-10-19 18:25:13 +0900

    open cv video

--- Camcha/app/src/main/cpp/native-lib.cpp
+++ Camcha/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,14 @@
 #include <jni.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 
 extern "C" {
     JNIEXPORT void JNICALL
+
+    Point minLoc,maxLoc;
+    double minVal,maxVal;
+
     Java_com_softmilktea_camcha_MainActivity_ConvertRGBtoGray(
             JNIEnv *env, jobject /* this */,
             jlong matAddrInput, jlong matAddrResult) {
@@ -13,6 +16,14 @@ extern "C" {
         Mat &matInput = *(Mat *)matAddrInput;
         Mat &matResult = *(Mat *)matAddrResult;
 
-        cvtColor(matInput, matResult, CV_RGBA2GRAY);
+        cvtColor(matInput, matResult, COLOR_BGR2GRAY);
+
+
+
+        GaussianBlur(matResult, matResult, Size(7,7), 1.5, 1.5);
+        
+        minMaxLoc(matResult, &minVal,&maxVal,&minLoc,&maxLoc,noArray());
+        circle(matResult, maxLoc, 10, 1000);
+        
     }
 }
