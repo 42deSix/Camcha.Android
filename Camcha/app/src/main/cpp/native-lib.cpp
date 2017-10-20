1cd9b59a986eed6675864714cde05d499424c9e0
Sejin Jeon <jinaidy93@gmail.com>
2017-10-20 16:04:26 +0900

    Modified implanted Opencv code to be able to work

--- Camcha/app/src/main/cpp/native-lib.cpp
+++ Camcha/app/src/main/cpp/native-lib.cpp
@@ -1,29 +1,32 @@
 #include <jni.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 
 using namespace cv;
 
 extern "C" {
     JNIEXPORT void JNICALL
 
-    Point minLoc,maxLoc;
-    double minVal,maxVal;
-
     Java_com_softmilktea_camcha_MainActivity_ConvertRGBtoGray(
             JNIEnv *env, jobject /* this */,
             jlong matAddrInput, jlong matAddrResult) {
 
+        Point minLoc,maxLoc;
+        double minVal,maxVal;
+
         Mat &matInput = *(Mat *)matAddrInput;
         Mat &matResult = *(Mat *)matAddrResult;
+        matResult = matInput;
+//        Mat &matResult = *(Mat *)matAddrInput;
+        cvtColor(matInput, matInput, COLOR_BGR2GRAY);
 
-        cvtColor(matInput, matResult, COLOR_BGR2GRAY);
 
 
-
-        GaussianBlur(matResult, matResult, Size(7,7), 1.5, 1.5);
+//        GaussianBlur(matInput, matResult, Size(7,7), 1.5, 1.5);
         
-        minMaxLoc(matResult, &minVal,&maxVal,&minLoc,&maxLoc,noArray());
-        circle(matResult, maxLoc, 10, 1000);
-        
+        minMaxLoc(matInput, &minVal,&maxVal,&minLoc,&maxLoc);
+        circle(matResult, maxLoc, 50, 1000, 5);
+
     }
 }
